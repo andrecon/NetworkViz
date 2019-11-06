fcfd2cfbb7fb7cfefdb1b55f44fe3636faa56300
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-11-06 13:59:45 -0800

    Plotly plots in two subplots, Source and Destination...Still needs work

--- networkvis.py
+++ networkvis.py
@@ -1,6 +1,9 @@
 from scapy.all import *
 from collections import Counter
 import plotly
+import plotly.offline
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 print("Sniffing...")
 packets = sniff(count=100)
@@ -36,8 +39,30 @@ for ip, count in cnt.most_common():
     xData.append(ip)
     yData.append(count)
 
-plotly.offline.plot({
-   "data":[plotly.graph_objs.Bar(x=xData, y=yData)],
-"layout":plotly.graph_objs.Layout(title="Source IP Occurrence",
-xaxis=dict(title="SRC IP"),
-       yaxis=dict(title="Count"))})
+# plotly.offline.plot(
+#     {
+#         "data":[plotly.graph_objs.Bar(x=xData, y=yData)], 
+#         "layout":plotly.graph_objs.Layout(title="Source IP Occurrence",
+#         xaxis=dict(title="SRC IP"),
+#         yaxis=dict(title="Count"))
+#     }
+#     )
+
+fig = make_subplots(
+    rows=2, cols=2,
+    subplot_titles=("Number of Packets for Source", "Number of Packets for Destination")
+    )
+
+
+fig.add_trace( go.Bar(x=xData, y=yData, name="Source"),
+        row=1, col=1,
+        )   
+
+fig.add_trace(
+        go.Bar(x=xData, y=yData, name="Destination"), 
+        row=1, col=2,
+        )
+
+fig.update_layout(height=1300, width=1300, title_text="Network Visualization")
+fig.show()
+
