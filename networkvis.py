479c95e7523cb77c62ce4f310ebe0f6cfa078c8c
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-12-09 08:46:04 -0800

    Fixed top three info

--- networkvis.py
+++ networkvis.py
@@ -266,15 +266,10 @@ if __name__ == '__main__':
     root.bind('<Escape>', exit)
 
 
-    # scrollbar = tk.Scrollbar(root)
-    # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
     # listbox = tk.Listbox(root, yscrollcommand=scrollbar.set)
     # for i in range(1000):
     #     listbox.insert(tk.END, str(i))
     # listbox.pack(side=tk.LEFT, fill=tk.BOTH)
-
-    # scrollbar.config(command=listbox.yview)
 
     # while True:
     #     try:
